f5be0b88884ad26ed61941a8385b049f609ae8e0
Alex Forrester <alexforrester96@gmail.com>
2019-10-24 14:30:53 +0100

    finsihed mvp

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div v-if="chartData.length">
+    <h1>Energy Chart</h1>
+    <GChart
+    type="PieChart"
+    :data="chartData"
+    :options="chartOptions"
+    :resizeDebounce=1000
+    />
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {eventBus} from './main.js'
+import {GChart} from "vue-google-charts"
+import VueGoogleCharts from 'vue-google-charts'
+
 
 export default {
   name: 'app',
+  data(){
+    return {
+      chartData: [],
+      mappedData: [],
+      chartOptions: {
+        chart: {
+          title: 'Company Performance',
+          subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+        }
+      }
+    }
+  },
+  mounted(){
+    fetch('https://api.carbonintensity.org.uk/generation')
+    .then(response => response.json())
+    .then(res => this.chartData = [['header 1', 'Fuel Consumption'], ...this.transformData(res.data.generationmix)])
+
+  },
+  methods: {
+    transformData(arr){
+      return arr.map((energy) => {
+        return [energy.fuel, energy.perc]
+      })
+    }
+  },
   components: {
-    HelloWorld
+    GChart
   }
 }
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+export const eventBus = new Vue();
+import VueGoogleCharts from 'vue-google-charts'
+
+Vue.use(VueGoogleCharts);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
